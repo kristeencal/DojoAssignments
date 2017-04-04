f0b5ea2f3964aac5d40d902bd4b72b6c490f639d
ChristineI <kristeen.cal@gmail.com>
2017-04-03 23:28:24 -0700

    MEAN Day 1 assignments

---
+++ MEAN/buildSomeFunctions.js
@@ -0,0 +1,51 @@
+var run;
+function runningLogger () {
+  var run = 'I am running!'
+  console.log(run);
+}
+runningLogger ();
+
+var parameter;
+function multiplyByTen (parameter) {
+  console.log(parameter*10);
+}
+multiplyByTen (5);
+
+function stringReturnOne () {
+  var string1= 'I love coding';
+  return string1;
+}
+
+function stringReturnTwo () {
+  var string2= 'This is getting easier';
+  return string2;
+}
+
+// function caller (a) {
+//   if typeof(a) = function () {
+//     a = 'invoked';
+//   }
+// }
+
+// function myDoubleConsoleLog (a,b) {
+//   var a = function () {
+//     console.log(1);
+//   }
+//   var b = function () {
+//     console.log(2);
+//   }
+//   if a & b = function () {
+//     console.log (a,b);
+//   }
+// }
+
+function caller2 (a) {
+  var stringy = 'starting';
+  var 2 = 'ending';
+  console.log(stringy);
+  if typeof(a) = function () {
+    a = 'changed';
+  }
+  return ('interesting');
+}
+function caller2(myDoubleConsoleLog);
---
+++ MEAN/jsFunamentalsPart1.js
@@ -0,0 +1,9 @@
+var x = [3,5,"Dojo", "rocks", "Michael", "Sensei"]
+console.log(x);
+
+var x = [3,5,"Dojo", "rocks", "Michael", "Sensei", "Amy"]
+for (var i=0; i<x.length; i++) {
+  console.log(x[i]);
+}
+x.push(100);
+console.log(x);
