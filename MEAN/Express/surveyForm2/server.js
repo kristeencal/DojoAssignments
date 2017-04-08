e53c519091c2063f9edf83f870d1aa8be78a8ea6
ChristineI <kristeen.cal@gmail.com>
2017-04-07 19:24:18 -0700

    survey form update

--- MEAN/Express/surveyForm2/server.js
+++ MEAN/Express/surveyForm2/server.js
@@ -1,23 +1,28 @@
 var express = require("express");
-var app = express();
-var bodyParser = require('body-parser');
 var path = require("path");
+var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + "/static"));
+var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/static"));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+// var route = require("./routes/index.js")(app)
 
 app.get('/', function(req, res) {
  res.render("index");
 })
 
-app.post("/form", function(req, res)
+app.post("/result", function(req, res)
 {
-    var fullname = req.body.fullname;
-    res.send("Post successful");
+    var submitted_info = {
+      fullname: req.body.fullname
+      // value: req.body.value
+    }
+    console.log(req.body);
+    res.render("result", {user_data: submitted_info});
 });
-app.listen(8000, function() {
- console.log("listening on port 8000");
+app.listen(3000, function() {
+ console.log("listening on port 3000");
 });
